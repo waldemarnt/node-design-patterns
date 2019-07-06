89842d4863a7c3bfd43b7797d2746d867f5d8394
Waldemar Neto <waldemarnt@gmail.com>
2019-07-07 08:45:47 +1000

    Add example parameter for simple image example

--- examples/factory/__tests__/simple_image_factory.test.js
+++ examples/factory/__tests__/simple_image_factory.test.js
@@ -2,7 +2,7 @@ const { createImage, Image } = require('../simple_image_factory');
 
 describe('Image Factory', () => {
   it('should create a new instance of Image', () => {
-    const ImageInstance = createImage();
+    const ImageInstance = createImage('jpg');
     expect(ImageInstance).toBeInstanceOf(Image);
   });
 });
--- examples/factory/simple_image_factory.js
+++ examples/factory/simple_image_factory.js
@@ -4,8 +4,8 @@ class Image {}
  * Simple factory example just a function that constructs and object
  * and return it.
  */
-function createImage() {
-  return new Image();
+function createImage(type) {
+  return new Image(type);
 }
 
 // The Image class is being exported here just for test purposes
