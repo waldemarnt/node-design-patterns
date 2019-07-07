5115b63730eb85f193a7ed90fe06edc56cad80ce
Waldemar Neto <waldemarnt@gmail.com>
2019-07-07 20:11:05 +1000

    Fix code style

--- examples/factory/__tests__/simple_image_factory.test.js
+++ examples/factory/__tests__/simple_image_factory.test.js
@@ -1,7 +1,6 @@
 const { createImage, Image } = require('../simple_image_factory');
 
 describe('Image Factory', () => {
-
   it('should create a new instance of Image', () => {
     const ImageInstance = createImage('jpg');
     expect(ImageInstance).toBeInstanceOf(Image);
