fd200660fe072d39cbeece40666a85c858ae502f
Waldemar Neto <waldemarnt@gmail.com>
2019-07-07 20:08:45 +1000

    Adding spaces between test cases

--- examples/factory/__tests__/advanced_image_factory.test.js
+++ examples/factory/__tests__/advanced_image_factory.test.js
@@ -1,6 +1,7 @@
 const { createImage, Image } = require('../advanced_image_factory');
 
 describe('Advanced Image Factory', () => {
+
   it('should create an instance of Image given the type is not supported', () => {
     const ImageInstance = createImage('bmp');
     expect(ImageInstance).toBeInstanceOf(Image);
--- examples/factory/__tests__/simple_image_factory.test.js
+++ examples/factory/__tests__/simple_image_factory.test.js
@@ -1,6 +1,7 @@
 const { createImage, Image } = require('../simple_image_factory');
 
 describe('Image Factory', () => {
+
   it('should create a new instance of Image', () => {
     const ImageInstance = createImage('jpg');
     expect(ImageInstance).toBeInstanceOf(Image);
