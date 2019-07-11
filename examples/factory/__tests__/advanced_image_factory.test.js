01a237f135115f611b11faf9728ac9dac95e3940
Diogo Cezar <diogo@diogocezar.com>
2019-07-11 07:31:08 -0300

    Adjusting instances comparisons

--- examples/factory/__tests__/advanced_image_factory.test.js
+++ examples/factory/__tests__/advanced_image_factory.test.js
@@ -9,15 +9,18 @@ describe('Advanced Image Factory', () => {
   it('should create an instance of JPG given the type is jpg', () => {
     const ImageInstance = createImage('jpg');
     expect(ImageInstance).toBeInstanceOf(JPGImage);
+    expect(ImageInstance).toBeInstanceOf(Image); // Also is an instance of Image
   });
 
   it('should create an instance of GIF given the type is gif', () => {
     const ImageInstance = createImage('gif');
     expect(ImageInstance).toBeInstanceOf(GIFImage);
+    expect(ImageInstance).toBeInstanceOf(Image); // Also is an instance of Image
   });
 
   it('should create an instance of PNG given the type is png', () => {
     const ImageInstance = createImage('png');
     expect(ImageInstance).toBeInstanceOf(PNGImage);
+    expect(ImageInstance).toBeInstanceOf(Image); // Also is an instance of Image
   });
 });
