a21e018e5b4fd024552e9eb31216a106bdeca3a7
Waldemar Neto <waldemarnt@gmail.com>
2019-07-14 19:44:58 +1000

    Add data pipeline class for middleware example

---
+++ examples/middleware/data_pipeline.js
@@ -0,0 +1,37 @@
+class DataPipeline {
+  constructor(middlewares, data) {
+    this.middlewares = middlewares;
+    /**
+     * Adds a static end function to the data object
+     */
+    data.end = function() {};
+    this.data = data;
+  }
+
+  /**
+   * @data stays in a closure here
+   */
+  dispatch() {
+    let iterator = 0;
+    if (iterator < this.middlewares.length) {
+      const firstMiddleware = this.middlewares[iterator];
+      /**
+       * next is a wrap function, this way the middleware doesn't need to
+       * pass data as parameter for the next middleware, the next() function
+       * is responsible for getting it from the closure scope and
+       * injecting for the next middleware
+       */
+      const next = () => {
+        if (iterator < this.middlewares.length) {
+          const nextMiddleware = this.middlewares[iterator++];
+          nextMiddleware(this.data, next);
+        } else {
+          this.data.end();
+        }
+      };
+      firstMiddleware(this.data, next);
+    }
+  }
+}
+
+module.exports = DataPipeline;
