b5a844232593041e2684205aa19ac2e8aac656f8
Waldemar Neto <waldemarnt@gmail.com>
2019-07-15 16:56:06 +1000

    Improve comments

--- examples/middleware/data_pipeline.js
+++ examples/middleware/data_pipeline.js
@@ -1,6 +1,7 @@
 class DataPipeline {
   constructor(middlewares, data) {
     this.middlewares = middlewares;
+    //Informs whether the pipiline has finished
     this.finished = false;
     /**
      * Adds a static end function to the data object
@@ -16,17 +17,19 @@ class DataPipeline {
    * @data stays in a closure here
    */
   dispatch() {
+    // Itarator used to iterate on the middlewares list
     let iterator = 0;
     if (iterator < this.middlewares.length) {
       const firstMiddleware = this.middlewares[iterator];
       /**
-       * next is a wrap function, this way the middleware doesn't need to
+       * The next() is a wrap function, this way the middleware doesn't need to
        * pass data as parameter for the next middleware, the next() function
        * is responsible for getting it from the closure scope and
        * injecting for the next middleware
        */
       const next = () => {
         if (!this.finished && iterator < this.middlewares.length) {
+          //Increases the iterator and get the next middleware
           const nextMiddleware = this.middlewares[iterator++];
           nextMiddleware(this.data, next);
         } else {
--- examples/middleware/middleware_manager.js
+++ examples/middleware/middleware_manager.js
@@ -2,15 +2,19 @@ const DataPipeline = require('./data_pipeline');
 
 class MiddlewareManager {
   constructor() {
+    //All the middlewares are stored on the MiddlewareManager's instance
     this.middlewares = [];
   }
 
   process(data = {}) {
+    //A new instance of DataPipeline is instantiate, it will keep the pipeline's state
     const dataPipeline = new DataPipeline(this.middlewares, data);
+    //Starts the flow
     dataPipeline.dispatch();
   }
 
   use(middleware) {
+    //Add the middlewares to the middlewares list
     this.middlewares.push(middleware);
   }
 }
