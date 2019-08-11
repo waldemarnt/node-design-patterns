69e2de4d423089fd738941ac9a66dbe0b2960a1f
Waldemar Neto <waldemarnt@gmail.com>
2019-08-11 17:32:12 +1000

    Add full Promise pattern example

--- examples/promise/__tests__/promise.test.js
+++ examples/promise/__tests__/promise.test.js
@@ -1,15 +1,106 @@
-const NPromise = require('../index');
+const NPromise = require('../new_promise');
 
 describe('Promise', () => {
-  it('should instantiate a new Promise with in pending state', done => {
-    const nPromise = new NPromise(resolve => {
-      setTimeout(() => resolve(true), 10);
+  it('should then a Promise', done => {
+    return new NPromise(resolve => resolve({ data: 'fake' })).then(response => {
+      expect(response.data).toBe('fake');
+      done();
     });
-    const testPromise = new Promise((resolve) => {
-      setTimeout(() => resolve(true), 10);
+  });
+
+  it('should call then just when the async code is resolved', done => {
+    return new NPromise(resolve => {
+      setTimeout(() => resolve({ data: 'fake' }), 10);
+    }).then(response => {
+      expect(response.data).toBe('fake');
+      done();
     });
-    setTimeout(() => console.log(testPromise), 1000)
-    setTimeout(() => console.log(nPromise), 1000)
-    setTimeout(() => done(), 2001)
+  });
+
+  it('should support a chain of promises', done => {
+    return new NPromise(resolve => {
+      setTimeout(() => resolve({ data: 'promise1' }), 10);
+    })
+      .then(response => {
+        expect(response.data).toBe('promise1');
+        return { data: 'promise2' };
+      })
+      .then(response => {
+        expect(response.data).toBe('promise2');
+        done();
+      });
+  });
+
+  it('should allow the same promise to be thenable multiple times', done => {
+    const p1 = new NPromise(resolve => setTimeout(() => resolve({ data: 'fake' }), 10));
+
+    p1.then(response => {
+      expect(response.data).toBe('fake');
+    });
+
+    p1.then(response => {
+      expect(response.data).toBe('fake');
+      done();
+    });
+  });
+
+  it('should support chain of promises on which promises are returned', done => {
+    const fakeFSPromise = new Promise(resolve => setTimeout(() => resolve({ file: 'photo.jpg' }), 10));
+    return new NPromise(resolve => {
+      setTimeout(() => resolve({ data: 'promise1' }), 10);
+    })
+      .then(response => {
+        expect(response.data).toBe('promise1');
+        return fakeFSPromise;
+      })
+      .then(response => {
+        expect(response.file).toBe('photo.jpg');
+        done();
+      });
+  });
+
+  describe('Error handling', () => {
+    it('should call catch when an error is thrown', done => {
+      const errorMessage = 'Promise has been rejected';
+
+      return new NPromise((resolve, reject) => {
+        setTimeout(() => reject(new Error(errorMessage)), 10);
+      }).catch(error => {
+        expect(error.message).toBe(errorMessage);
+        done();
+      });
+    });
+
+    it('should allow catch to be thenable', done => {
+      const errorMessage = 'Promise has been rejected';
+
+      return new NPromise((resolve, reject) => {
+        setTimeout(() => reject(new Error(errorMessage)), 10);
+      })
+        .catch(error => {
+          expect(error.message).toBe(errorMessage);
+          return { data: 'someData' };
+        })
+        .then(response => {
+          expect(response.data).toBe('someData');
+          done();
+        });
+    });
+
+    it('should allow catch an error thrown by a previous catch method', done => {
+      const errorMessage = 'Promise has been rejected';
+
+      return new NPromise((resolve, reject) => {
+        setTimeout(() => reject(new Error(errorMessage)), 10);
+      })
+        .catch(error => {
+          expect(error.message).toBe(errorMessage);
+          throw new Error('That is another error');
+        })
+        .catch(error => {
+          expect(error.message).toBe('That is another error');
+          done();
+        });
+    });
   });
 });
---
+++ examples/promise/new_promise.js
@@ -0,0 +1,141 @@
+const STATE = {
+  PENDING: 'PENDING',
+  FULFILLED: 'FULFILLED',
+  REJECTED: 'REJECTED',
+};
+/**
+ * NewPromise
+ */
+class NPromise {
+  /**
+   * Promise constructor
+   * http://www.ecma-international.org/ecma-262/6.0/#sec-promise-constructor
+   */
+  constructor(executor) {
+    if (typeof executor !== 'function') {
+      throw new Error('Executor must be a function');
+    }
+    this.state = STATE.PENDING;
+    this.value = undefined;
+    this.onFulfillChain = [];
+    this.onRejectCallChain = [];
+
+    /**
+     * Call(executor, undefined, «resolvingFunctions.[[Resolve]], resolvingFunctions.[[Reject]]»).
+     * It calls executor function passing the
+     * resolve and reject parameters to the promise itself
+     */
+    executor(this.resolve.bind(this), this.reject.bind(this));
+  }
+
+  then(onFulfill) {
+    /**
+     * A Promise always return a new Promise
+     */
+    return new NPromise(resolve => {
+      const onFulfilled = res => {
+        resolve(onFulfill(res));
+      };
+      /**
+       * If then was called from an already fulfilled promise
+       * it doens't need to wait and can resolve straight
+       */
+      if (this.state === STATE.FULFILLED) {
+        onFulfilled(this.value);
+      } else {
+        /**
+         * If the promise before this is in PENDING
+         * the fulfill callback will be pushed to a list
+         * to be resolved afterwards
+         */
+        this.onFulfillChain.push({ onFulfilled });
+      }
+    });
+  }
+
+  catch(onReject) {
+    return new NPromise((resolve, reject) => {
+      const onRejected = res => {
+        try {
+          resolve(onReject(res));
+        } catch (error) {
+          /**
+           * If an error in thrown inside a catch, it should
+           * reject and next catch will be called
+           */
+          reject(error);
+        }
+      };
+
+      /**
+       * If the promise is not fulfilled, which means its resolving something
+       * put it in an array to be resolved afterwards
+       */
+      if (this.state === STATE.REJECTED) {
+        onReject(this.value);
+      } else {
+        this.onRejectCallChain.push(onRejected);
+      }
+    });
+  }
+
+  /**
+   * Promise resolve function
+   * http://www.ecma-international.org/ecma-262/6.0/#sec-promise-resolve-functions
+   */
+  resolve(res) {
+    /**
+     * It prevents resolved to be called more than
+     * once for the same Promise
+     */
+    if (this.state !== STATE.PENDING) {
+      return;
+    }
+
+    /**
+     * In case when a Promise returns a promise in PENDING
+     * state, the next then has to be called
+     */
+    if (res != null && typeof res.then === 'function') {
+      return res.then(this.resolve.bind(this));
+    }
+
+    /**
+     * sets it's state to FULFILLED
+     */
+    this.state = STATE.FULFILLED;
+    this.value = res;
+
+    /**
+     * Resolves the all thens for a given Promise
+     */
+    for (const { onFulfilled } of this.onFulfillChain) {
+      onFulfilled(res);
+    }
+  }
+
+  /**
+   * Promise reject function
+   * http://www.ecma-international.org/ecma-262/6.0/#sec-promise-reject-functions
+   */
+  reject(error) {
+    /**
+     * if not pending return
+     */
+    if (this.state !== STATE.PENDING) {
+      return;
+    }
+
+    this.state = STATE.REJECTED;
+    this.value = error;
+
+    /**
+     * call all the catchs for a given Promise
+     */
+    for (const onRejected of this.onRejectCallChain) {
+      onRejected(error);
+    }
+  }
+}
+
+module.exports = NPromise;
