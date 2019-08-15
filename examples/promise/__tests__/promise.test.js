9298d1e288563a637f14c2b12eb2375b8199e18c
Waldemar Neto <waldemarnt@gmail.com>
2019-08-16 07:45:10 +1000

    Add new Promise test case

--- examples/promise/__tests__/promise.test.js
+++ examples/promise/__tests__/promise.test.js
@@ -2,6 +2,12 @@ const NPromise = require('../new_promise');
 
 describe('Promise', () => {
   describe('When fulfilled', () => {
+    it('should create a new Promise with pending state', () => {
+      const promise = new NPromise(() => {});
+      expect(promise.state).toBe('pending');
+      expect(promise.value).toBe(undefined);
+    });
+
     it('should resolve a Promise syncronously with a given value', () => {
       const promise = new NPromise(resolve => resolve({ data: 'fake' }));
       expect(promise.state).toBe('fulfilled');
