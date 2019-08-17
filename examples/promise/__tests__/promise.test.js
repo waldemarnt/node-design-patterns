46a0184ecfba499e8329deeb036e1d0835409f78
Waldemar Neto <waldemarnt@gmail.com>
2019-08-17 17:30:10 +1000

    Move promise test case to correct space

--- examples/promise/__tests__/promise.test.js
+++ examples/promise/__tests__/promise.test.js
@@ -1,13 +1,13 @@
 const NPromise = require('../new_promise');
 
 describe('Promise', () => {
-  describe('When fulfilled', () => {
-    it('should create a new Promise with pending state', () => {
-      const promise = new NPromise(() => {});
-      expect(promise.state).toBe('pending');
-      expect(promise.value).toBe(undefined);
-    });
+  it('should create a new Promise with pending state', () => {
+    const promise = new NPromise(() => {});
+    expect(promise.state).toBe('pending');
+    expect(promise.value).toBe(undefined);
+  });
 
+  describe('When fulfilled', () => {
     it('should resolve a Promise syncronously with a given value', () => {
       const promise = new NPromise(resolve => resolve({ data: 'fake' }));
       expect(promise.state).toBe('fulfilled');
